a1bed50d79b4ddf2b93f59643e9443372e647133
Slopcorn <raido.everest@protonmail.ch>
2020-07-09 21:49:45 +0300

    GUI iteration

--- src/life/GameOfLife.java
+++ src/life/GameOfLife.java
@@ -7,7 +7,23 @@ public class GameOfLife extends JFrame {
         super("Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
-        setLayout(null);    // Center window
+
+        // Main panel
+        JPanel panel = new JPanel();
+        add(panel);
+
+        // Creating the labels that give info about which generation it is and how many cells are alive.
+        JLabel generation = new JLabel();
+        JLabel alive      = new JLabel();
+        generation.setName("GenerationLabel");
+        alive.setName("AliveLabel");
+        panel.add(generation);
+        panel.add(alive);
+
+        generation.setText("generation");
+        alive.setText("alive");
+
+
         setVisible(true);
     }
 }
