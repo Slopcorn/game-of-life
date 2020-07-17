57266b3ba0b7aa6f02a7e2efc6d2bc2634f6c360
Slopcorn <raido.everest@protonmail.ch>
2020-07-17 16:41:25 +0300

    Interface tweaks

--- src/life/GameOfLife.java
+++ src/life/GameOfLife.java
@@ -7,9 +7,12 @@ public class GameOfLife extends JFrame {
         super("Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 500);
+        setResizable(false);
 
         // Main panel
         JPanel panel = new JPanel();
+        panel.setSize(500, 500);
+        panel.setLayout(null);
         add(panel);
 
         // Creating the labels that give info about which generation it is and how many cells are alive.
@@ -17,12 +20,15 @@ public class GameOfLife extends JFrame {
         JLabel alive      = new JLabel();
         generation.setName("GenerationLabel");
         alive.setName("AliveLabel");
+        generation.setBounds(10, 0, 200, 20);
+        alive.setBounds(10, 25, 200, 20);
         panel.add(generation);
         panel.add(alive);
 
         // Adding the game field itself
         Universe universe = new Universe();
         universe.setName("GameWorld");
+        universe.setBounds(50, 50, 400, 400);
         panel.add(universe);
         panel.repaint();
 
